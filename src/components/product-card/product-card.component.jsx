23e6598f7fc012cb8e1ab5d16478ad4e651978b1
Nicolas Ovejero <nicolas.ovejero.epitech@gmail.com>
2022-09-08 15:54:28 -0400

    Feat: add item(s) in cart dropdown

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,11 @@
 import './product-card.style.scss';
-
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
 import Button from '../button/button.component';
 
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);
 
     return (
         <div className='product-card-container'>
@@ -16,7 +18,7 @@ const ProductCard = ({ product }) => {
                     {price}
                 </span>
             </div>
-            <Button buttonType='inverted'>Add to card</Button>
+            <Button buttonType='inverted' onClick={() => addItemToCart(product)}>Add to card</Button>
         </div>
     );
 }
