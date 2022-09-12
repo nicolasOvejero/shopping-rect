57a6a05cffce742f96cf63219e1ed8dc6802abaf
Nicolas Ovejero <nicolas.ovejero.epitech@gmail.com>
2022-09-12 15:29:17 -0400

    Feat: Add redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,21 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const middlewares = [ logger ];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    blacklist: [ 'user' ]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
